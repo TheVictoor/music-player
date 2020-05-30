fc3c0b341e166aa4f369629e99cd466509db5182
TheVictoor <victoorsp@gmail.com>
2020-05-30 17:07:39 -0300

    Refactor - Converting class component to function component

--- src/views/albums/index.js
+++ src/views/albums/index.js
@@ -1,49 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import AlbumArt from '../../components/album-art-simple';
 import services from '../../services';
 import './index.css';
 
-class Albums extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			albums: [],
+
+const Albums = () => {
+	const [albums, setAlbums] = useState([]);
+
+	useEffect(() => {
+		const fetchAlbums = async () => {
+				const albums = await services.albums.recomendation();
+				setAlbums(albums);
 		};
-	}
 
-	async componentDidMount(){
-		const albums = await services.albums.recomendation();
-		this.setState({ albums });
-	}
+		fetchAlbums();
+	}, []);
 
-	render(){
+	const albumsComponents = albums.map(album => {
 		return (
-			<div className="almbum-container d-flex justify-content-center">
-				<div className='albums'>
-					<div>
-						<h1 className='text-center header-albums py-2'>
-							Albums
-						</h1>
-					</div>
-					<div className=''>
-						{
-							this.state.albums.map(a => {
-								return (
-									<div className="d-inline-block" key={a.id}>
-										<Link to={`/player/${a.id}`} className='link-color'>
-											<AlbumArt album={a}/>
-										</Link>
-									</div>
-								)
-							})
-						}
-					</div>
+			<div className="d-inline-block" key={album.id}>
+				<Link to={`/player/${album.id}`} className='link-color'>
+					<AlbumArt album={album}/>
+				</Link>
+			</div>
+		)
+	});
+
+	return (
+		<div className="almbum-container d-flex justify-content-center">
+			<div className='albums'>
+				<div>
+					<h1 className='text-center header-albums py-2'>
+						Albums
+					</h1>
+				</div>
+				<div className=''>
+					{ albumsComponents }
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Albums;
