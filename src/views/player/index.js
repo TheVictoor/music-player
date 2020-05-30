ca42a5d008182712b3027b31c391f901292e84d6
TheVictoor <victoorsp@gmail.com>
2020-05-30 17:40:40 -0300

    Refactor - converting class component to function component at player view

--- src/views/player/index.js
+++ src/views/player/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import AlbumArt from '../../components/album-art';
 import Controls from '../../components/music-controls';
@@ -9,40 +9,35 @@ import services from "../../services";
 
 import './index.css';
 
-class Player extends React.Component{
-	constructor(props){
-		super(props);
+const Player = (props) => {
+	const [id] = useState(props.match.params.id.toString());
+	const [album, setAlbum] = useState({});
+	const [track, setTrack] = useState({});
+	const updateTrack = (t) => { this.setState({ track: t }) };
 
-		this.state = {
-			album: {},
-			track: {},
-			id: props.match.params.id.toString(),
+	useEffect(() => {
+		const fetch = async () => {
+			const album = await services.albums.album(id);
+			const [track] = album.tracks;
+
+			setAlbum(album);
+			setTrack(track);
+
+			sessionStorage.setItem(id, JSON.stringify(album));
 		};
+		
+		fetch();
+		console.log('tests');
+	}, [id] );
 
-		this.updateTrack = (t) => { this.setState({ track: t }) };
-	}
-
-	async componentDidMount(){
-		const album = await services.albums.album(this.state.id);
-
-		this.setState({
-			album,
-			track: album.tracks[0]
-		});
-
-		sessionStorage.setItem(this.state.id, JSON.stringify(album));
-	}
-
-	render(){
-		return (
-			<div className="player d-flex flex-column align-items-center justify-content-center">
-				<PlayerNagivation id={this.state.id}/>
-				<AlbumArt album={this.state.album} currentTrack={this.state.track}/>
-				<Timer track={this.state.track} />
-				<Controls onUpdateTrack={this.updateTrack}/>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="player d-flex flex-column align-items-center justify-content-center">
+			<PlayerNagivation id={id}/>
+			<AlbumArt album={album} currentTrack={track}/>
+			<Timer track={track} />
+			<Controls onUpdateTrack={updateTrack}/>
+		</div>
+	);
+};
 
 export default Player;
